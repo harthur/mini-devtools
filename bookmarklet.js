82da015b1f800bdf3f2d2ce024c4bc2b1311c68d
Heather Arthur <fayearthur@gmail.com>
2014-09-18 16:16:44 -0400

    add bookmarklet

---
+++ bookmarklet.js
@@ -0,0 +1,6 @@
+javascript:(function(){
+  toolsScript=document.createElement('SCRIPT');
+  toolsScript.type='text/javascript';
+  toolsScript.src='http://harthur.github.io/mini-devtools/mini-tools.js';
+  document.getElementsByTagName('head')[0].appendChild(toolsScript);
+})();
