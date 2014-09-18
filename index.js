237da6b383741627affa0698e9ff9151177840f7
Heather Arthur <fayearthur@gmail.com>
2014-09-18 17:09:05 -0400

    change to be a magic 8 ball

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
   console.log("loaded button");
 
   $(".button").click(function() {
-    throw new Error("this is a super super super super super bad error");
+    //throw new Error("this is a super super super super super bad error");
+
+    $(".chosen-tools").addClass("show");
   });
 });
